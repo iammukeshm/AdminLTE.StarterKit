f5a49f4d26ab1805105f11a372fd3ec90c3f3e31
Mukesh Murugan <iammukeshm@gmail.com>
2020-07-13 02:19:13 +0530

    added login page

---
+++ AdminLTE.StarterKit/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTE.StarterKit.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
